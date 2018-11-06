7cb80b5e477294425c7f46a81f0cd6a2086205da
Nicolas Chachereau <nicolas@nchachereau.ch>
2018-11-06 16:45:36 +0100

    Commit initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,80 @@
+
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Index numérique de la Feuille officielle suisse du commerce (1883-2001)</title>
+        <link rel="stylesheet" href="css/app.css" type="text/css" media="screen">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    </head>
+    <body>
+        <section class="section is-medium">
+            <div class="container">
+                <h1 class="title is-2">Index numérique de la Feuille officielle suisse du commerce (1883-2001)</h1>
+                <p class="content">Grâce à sa numérisation, la
+                    <a href="https://www.e-periodica.ch/digbib/volumes?UID=sha-001"><i>Feuille officielle suisse du commerce</i> (FOSC)</a>
+                    est appelée à devenir une ressource importante pour étudier l’histoire économique et sociale de la Suisse contemporaine.
+                    À l’usage, il manque pourtant la possibilité d’obtenir rapidement la référence bibliographique de la page trouvée, ainsi
+                    que d’ouvrir la page correspondant à une référence. En somme, ce qu’il manque, c’est un index numérique.
+                </p>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <h2 class="title is-4">Obtenir la référence d’une page</h2>
+                <form method="get" action="">
+                    <div class="columns">
+                        <div class="field column">
+                            <label class="label" for="url">Adresse URL de la page numérisée sur e-periodica :</label>
+                            <div class="control is-expanded">
+                                <input class="input" name="url" id="url" type="text" value="" placeholder="https://www.e-periodica.ch/digbib/view?pid=sha-001:1887:5#86">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="control">
+                        <input class="button is-primary" type="submit" value="Obtenir la référence">
+                    </div>
+                </form>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <h2 class="title is-4">Trouver une page</h2>
+                <form action="">
+                    <div class="columns">
+                        <div class="column">
+                            <label class="label" for="dt">Année ou date :</label>
+                            <div class="control">
+                                <input class="input" name="dt" id="dt" type="text" required placeholder="1887">
+                            </div>
+                        </div>
+                        <div class="column">
+                            <label class="label" for="n">Cahier :</label>
+                            <div class="control">
+                                <input class="input" name="n" id="n" type="text" placeholder="10">
+                            </div>
+                        </div>
+                        <div class="column">
+                            <label class="label" for="p">Page :</label>
+                            <div class="control">
+                                <input class="input" name="p" id="p" type="text" required placeholder="73">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="control">
+                            <input class="button is-primary" type="submit" value="Ouvrir la page">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <footer class="footer">
+            <div class="container">
+                <div class="content">
+                    <p>Une création de <a href="#">Nicolas Chachereau</a>.</p>
+                    <p>Site construit en PHP grâce au <i lang="en">framework</i> <a href="https://laravel.com/">Laravel</a>. Style basé sur <a href="https://bulma.io/">Bulma</a>. Le code source est disponible sur <a href="#"><i class="fab fa-github"></i> Github</a>.</p>
+                </div>
+            </div>
+        </footer>
+    </body>
